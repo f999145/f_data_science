c31dc5072eec798f0260c31c9fb0951afa6d9e7a
f999145 <f999145@yandex.ru>
2022-07-03 15:57:57 +0300

    Тест работы словаря в новых версиях python

---
+++ Project_02/SF_module_10.py
@@ -0,0 +1,6 @@
+# Напоминаем способ создания словаря через список кортежей
+# (ключ, значение)
+if False:
+    data = [('Ivan', 19),('Mark', 25),('Andrey', 23),('Maria', 20)]
+    client_ages = dict(data)
+    print(client_ages)
