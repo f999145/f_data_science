633bae1d74520cf16fda1b8a415e3f16ac9aad7f
f999145 <f999145@yandex.ru>
2022-07-04 22:09:59 +0300

    Задание 4.10 defaultdict(deque)

--- Project_02/SF_module_10.py
+++ Project_02/SF_module_10.py
@@ -47,3 +47,39 @@ if False:
     # То есть это свернутая форма цикла в цикле
     print(north_list)
 
+# Напишите функцию task_manager, которая принимает список задач 
+# для нескольких серверов. Каждый элемент списка состоит из кортежа 
+# (<номер задачи>, <название сервера>, <высокий приоритет задачи>).
+
+# Функция должна создавать словарь и заполнять его задачами 
+# по следующему принципу: название сервера — ключ, 
+# по которому хранится очередь задач для конкретного сервера. 
+# Если поступает задача без высокого приоритета 
+# (последний элемент кортежа — False), добавить номер задачи 
+# в конец очереди. Если приоритет высокий, добавить номер в начало.
+
+# Для словаря используйте defaultdict, для очереди — deque.
+
+# Функция возвращает полученный словарь с задачами.
+
+if False:
+    tasks = [(36871, 'office', False),
+    (40690, 'office', False),
+    (35364, 'voltage', False),
+    (41667, 'voltage', True),
+    (33850, 'office', False)]
+
+    def task_manager(tasks):
+        from collections import defaultdict
+        from collections import deque
+        dict_tasks = defaultdict(deque)
+        for num_task,name_s,pri_s in tasks:
+            if pri_s:
+                dict_tasks[name_s].appendleft(num_task)
+            else:
+                dict_tasks[name_s].append(num_task)
+        return dict_tasks
+            
+    print(task_manager(tasks))
+
+
