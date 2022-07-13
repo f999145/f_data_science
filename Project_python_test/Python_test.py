0d7469c7c05fbf45633e69a0dfadd04c14fa2df7
f999145 <f999145@yandex.ru>
2022-07-13 13:25:47 +0300

    test

--- Project_python_test/Python_test.py
+++ Project_python_test/Python_test.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-workers = ['Ivan', 'Nikita', 'Maria', 'John', 'Kate']
- 
-choice = np.random.choice(workers, size=(2,2), replace=False)
-print(choice)
-table = np.random.randint(1, 101, (3,2), np.uint8)
-print(np.info(table))
+simplelist = [19, 242, 14, 152, 142, 1000]
+
+print(np.mean(simplelist))
