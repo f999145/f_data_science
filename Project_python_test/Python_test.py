b7ac4ebe685f41b626e0e99bbfc5c7f7f8c0417c
f999145 <f999145@yandex.ru>
2022-07-13 12:51:27 +0300

    Test

--- Project_python_test/Python_test.py
+++ Project_python_test/Python_test.py
@@ -4,3 +4,5 @@ workers = ['Ivan', 'Nikita', 'Maria', 'John', 'Kate']
  
 choice = np.random.choice(workers, size=(2,2), replace=False)
 print(choice)
+table = np.random.randint(1, 101, (3,2), np.uint8)
+print(np.info(table))
